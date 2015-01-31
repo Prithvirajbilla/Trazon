7fb7b9c8e2568078e75f211254bac52228fd1656
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2015-01-31 14:51:02 +0530

    added static

---
+++ donor/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse,HttpResponseRedirect,Http404,HttpResponseNotFound
+
+# Create your views here.
+
+def home(request):
+	template = "base.html"
+	return render(request,template)
